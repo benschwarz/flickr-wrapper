fd5a2e1030131a1c2eaa5977dc0b4dfc0094a5c6
Ben <ben@Homeslice.local>
2008-03-18 06:57:29 +1100

    Cleaning up set class for specs

--- lib/flickr-wrapper/photoset.rb
+++ lib/flickr-wrapper/photoset.rb
@@ -1,26 +1,38 @@
 class Flickr::PhotoSet < Flickr::Base
   attr_accessor :id, :title, :description, :photo_count
   
-  def initialize(id, title, description, photo_count)
+  def initialize id, title, description, photo_count
     @id, @title, @description, @photo_count = id, title, description, photo_count
   end
   
   # Class methods require scope of the user, it is to be sent into the method
-  def self.list(caller)
-    flickr = Flickr::Query.new caller.user_id
-    (flickr.execute('flickr.photosets.getList')/:photosets).map do |set|
-      self.new(set[:id], set[:title], (set/:description).inner_html, set[:photos])
-    end
+  def self.list caller
+    query = Flickr::Query.new caller.user_id
+    parse(query.execute('flickr.photosets.getList'))
   end
   
   # Get information regarding set by searching with the set's ID
   def self.find caller, id
     query = Flickr::Query.new(caller.user_id).execute('flickr.photosets.getInfo', :photoset_id => id)
-    photosets = (query/:photoset)
-    photosets.empty? ? [] : photosets.map do |set|
-      self.new(set[:id], (set/:title).inner_text, (set/:description).inner_text, set[:photos])
+    # Find should return a single entity
+    parse(query).first
+  end
+  
+  # Get the photos within a set 
+  # Queries:
+  # > photosets.getPhotos (1 call)
+  # > Photo.find ID (n calls)
+  def photos
+    (Flickr::Query.new(user_id).execute('flickr.photosets.getPhotos', :photoset_id => id)/:photo).map do |photo|
+      Flickr::Photo.find user_id photo[:id]
     end
-    
-    return photosets
+  end
+  
+  private 
+  def self.parse collection
+    photosets = (collection/:photoset)
+    photosets.empty? ? [] : photosets.map do |set|
+      new(set[:id], (set/:title).inner_text, (set/:description).inner_text, set[:photos])
+    end
   end
 end
