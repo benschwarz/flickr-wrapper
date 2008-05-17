4f9eb769ccb748463b41efeb588e1d269d5688e9
Ben Schwarz <ben@Spinners.local>
2008-05-17 17:21:48 +1000

    Added spec for find_by_username Added and commented out specs to check if a user is pro or not

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -6,8 +6,24 @@ describe Flickr::User, "class" do
     @user.should be_an_instance_of Flickr::User
   end
   
+  it "should be finderable by username" do
+    @user = Flickr::User.find_by_username 'thomashawk'
+    @user.should be_an_instance_of Flickr::User
+  end
+  
   it "should have the users' realname" do
     @user = Flickr::User.find_by_email 'ben.schwarz@gmail.com'
     @user.realname.should eql "Ben Schwarz"
   end
+
+  it "should be a pro" do
+    @user = Flickr::User.find_by_username 'thomashawk'
+    @user.pro?.should be_true
+  end
+  
+  # it "should not be a pro" do
+  #   @user = Flickr::User.find_by_email 'ben.schwarz@gmail.com'
+  #   @user.pro?.should be_false
+  # end
+  
 end
